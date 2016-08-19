3ab470d3f5c1cdc2b47eef039ba2cde9577be6b1
tonytran1 <tonylyfe@gmail.com>
2016-08-18 21:18:56 -0700

    Added tooltip to titles, thumbnails, and player icon

--- js/search.js
+++ js/search.js
@@ -1,27 +1,28 @@
 $( document ).ready(function() {
-    $('.search-button').click(function(e) {
-      e.preventDefault();
-       // Prepare the request
-       var request = gapi.client.youtube.search.list({
-            part: "snippet",
-            type: "video",
-            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
-            maxResults: 48,
-            order: "viewCount"
-       });
-       request.execute(function(response) {
-         $("#content").html("<div class='col-lg-12'><h2 class='result-header page-header'>Results</h2></div>");
-         let results = response.result;
-         console.log(results.items[0]);
-         if (results.items[0] !== undefined) {
-           $.each(results.items, function(index, item) {
-             $("#content").append("<div class='col-lg-3 col-md-4 col-xs-6 thumb'><p class='video-title center'>" + setTitleLength(item.snippet.title) + "</p><a href='#/' onclick='return onSelect(\""+item.id.videoId+"\");' class='thumbnail picture'><img class='img-responsive' src=" + item.snippet.thumbnails.high.url + " alt=''></a></div>");
-           });
-         } else {
-           $("#content").append("<h3 class='center'>No Results!</h3>");
-         }
-       })
-    });
+  $('[data-toggle="tooltip"]').tooltip();
+  $('.search-button').click(function(e) {
+    e.preventDefault();
+     // Prepare the request
+     var request = gapi.client.youtube.search.list({
+          part: "snippet",
+          type: "video",
+          q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+          maxResults: 48,
+          order: "viewCount"
+     });
+     request.execute(function(response) {
+       $("#content").html("<div class='col-lg-12'><h2 class='result-header page-header'>Results</h2></div>");
+       let results = response.result;
+       console.log(results.items[0]);
+       if (results.items[0] !== undefined) {
+         $.each(results.items, function(index, item) {
+           $("#content").append("<div class='col-lg-3 col-md-4 col-xs-6 thumb'><p class='video-title center' data-toggle='tooltip' data-placement='top' title='" + item.snippet.title + "'>" + setTitleLength(item.snippet.title) + "</p><a href='#/' onclick='return onSelect(\""+item.id.videoId+"\");' class='thumbnail picture'><img class='img-responsive' src=" + item.snippet.thumbnails.high.url + " alt='' data-toggle='tooltip' data-placement='top' title='" + item.snippet.title + "'></a></div>");
+         });
+       } else {
+         $("#content").append("<h3 class='center'>No Results!</h3>");
+       }
+     })
+  });
 });
 
 function onSelect(videoId) {
@@ -46,18 +47,18 @@ function setTitleLength(title) {
 }
 
 function init() {
-    gapi.client.setApiKey("YouTube API Key");
-    gapi.client.load("youtube", "v3", function() {});
+  gapi.client.setApiKey("YouTube API Key");
+  gapi.client.load("youtube", "v3", function() {
+      // YouTube API ready
+  });
 }
 
 $("#player").click(function() {
   if ($("#audio-title").html() === "") {
     $("#audio-title").html("<i class='glyphicon glyphicon-volume-off' style='float:left'></i>Player");
-    $("#audio-body").html("<b>Please select a video to play.</b><br><br><button class='btn-primary btn-block' type='button' data-dismiss='modal'>Close</button>");
+    $("#audio-body").html("<b>Search and select a video to play.</b><br><br><button class='btn-primary btn-block' type='button' data-dismiss='modal'>Close</button>");
     $("#audio-modal").modal('show');
   } else {
     $("#audio-modal").modal('show');
   }
 })
-
-}
