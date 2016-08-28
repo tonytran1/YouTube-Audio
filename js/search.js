78a87c54e3edba79d79cdee13dc0833f07f269f6
tonytran1 <tonylyfe@gmail.com>
2016-08-28 16:28:24 -0700

    Updated UI and improved audio player responsiveness

--- js/search.js
+++ js/search.js
@@ -1,50 +1,53 @@
 $( document ).ready(function() {
   $('[data-toggle="tooltip"]').tooltip();
+
+  $(document).keypress(function(e) {
+    if (e.which == 13) {
+      obtainView();
+    }
+  });
+
   $('.search-button').click(function(e) {
     e.preventDefault();
-     // Prepare the request
-     var request = gapi.client.youtube.search.list({
-          part: "snippet",
-          type: "video",
-          q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
-          maxResults: 48,
-          order: "viewCount"
-     });
-     request.execute(function(response) {
-       $("#content").html("<div class='col-lg-12'><h2 class='result-header page-header'>Results</h2></div>");
-       var results = response.result;
-       if (results.items[0] !== undefined) {
-         $.each(results.items, function(index, item) {
-           $("#content").append("<div class='col-lg-3 col-md-4 col-xs-6 thumb'><p class='video-title center' data-toggle='tooltip' data-placement='top' title='" + item.snippet.title + "'>" + setTitleLength(item.snippet.title) + "</p><a href='#/' onclick='return onSelect(\""+item.id.videoId+"\");' class='thumbnail picture'><img class='img-responsive' src=" + item.snippet.thumbnails.high.url + " alt='' data-toggle='tooltip' data-placement='top' title='" + item.snippet.title + "'></a></div>");
-         });
-       } else {
-         $("#content").append("<h3 class='center'>No Results!</h3>");
-       }
-     })
+    obtainView();
   });
 
   $(".player").on('click', function() {
     if ($("#audio-title").html() === "") {
       $("#audio-title").html("<i class='glyphicon glyphicon-volume-off' style='float:left'></i>Player");
-      $("#audio-body").html("<b>Search and select a video to play.</b><br><br><button class='btn-primary btn-block' type='button' data-dismiss='modal'>Close</button>");
+      $("#audio-body").html("<b>Search and select a video to play.</b><br><br>");
       $("#audio-modal").modal('show');
     } else {
       $("#audio-modal").modal('show');
     }
-  })
+  });
 
 });
 
 function onSelect(videoId) {
   $.getJSON("https://www.youtubeinmp3.com/fetch/?format=JSON&bitrate=1&video=http://www.youtube.com/watch?v=" + videoId, function(response) {
     $("#audio-title").html("<i class='glyphicon glyphicon-music' style='float:left'></i>" + response.title);
-    $("#audio-body").html("<audio controls><source src=" + response.link + " type='audio/mpeg' /><a href="+ response.link +">"+response.title+"</a>An html5-capable browser is required to play this audio. </audio><br><a href="+ response.link +" download>Download</a><br><br><button class='btn-primary btn-block' type='button' data-dismiss='modal'>Close</button>");
+    $("#audio-body").html("<img src='assets/loading.gif'  width='50' height='50'>");
+    var audio = new Audio();
+    audio.setAttribute('src', response.link);
+    audio.setAttribute('class', 'center');
+    audio.controls = true;
+    audio.preload = 'metadata';
+    audio.oncanplay = function() {
+      $("#audio-body").html(audio);
+      $("#audio-body").append("<br><a href="+ response.link +" download>Download</a><br>");
+    };
+    audio.onerror = function() {
+      onSelect(videoId);
+    }
   })
   .fail(function() {
     $("#audio-title").html("<i class='glyphicon glyphicon-volume-off' style='float:left'></i>Sorry!");
-    $("#audio-body").html("<b>Unfortunately, this video is not supported for stream.<br>Please try the link below to convert your video.</b><br><a href=https://www.youtubeinmp3.com/fetch/?format=JSON&bitrate=1&video=http://www.youtube.com/watch?v=" + videoId +" target='_blank'>Convert</a><br><br><button class='btn-primary btn-block' type='button' data-dismiss='modal'>Close</button>");
+    $("#audio-body").html("<b>Unfortunately, this video is not supported for stream.<br>Please try the link below to convert your video.</b><br><a href=https://www.youtubeinmp3.com/fetch/?format=JSON&bitrate=1&video=http://www.youtube.com/watch?v=" + videoId +" target='_blank'>Convert</a><br><br>");
+  })
+  .always(function () {
+    $("#audio-modal").modal('show');
   });
-  $("#audio-modal").modal('show');
 }
 
 function setTitleLength(title) {
@@ -53,6 +56,28 @@ function setTitleLength(title) {
   } else {
     return title.replace(/^(.{20}[^\s]*).*/, "$1") + " ...";
   }
+}
+
+function obtainView() {
+  // Prepare the request
+  var request = gapi.client.youtube.search.list({
+       part: "snippet",
+       type: "video",
+       q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+       maxResults: 50,
+       order: "viewCount"
+  });
+  request.execute(function(response) {
+    $("#content").html("<div class='col-lg-12'><h2 class='result-header page-header'>Results</h2></div>");
+    var results = response.result;
+    if (results.items[0] !== undefined) {
+      $.each(results.items, function(index, item) {
+        $("#content").append("<div class='col-lg-3 col-md-4 col-xs-6 thumb'><p class='video-title center' data-toggle='tooltip' data-placement='top' title='" + item.snippet.title + "'>" + setTitleLength(item.snippet.title) + "</p><a href='#/' onclick='return onSelect(\"" + item.id.videoId + "\");' class='thumbnail picture'><img class='img-responsive' src=" + item.snippet.thumbnails.high.url + " alt='' data-toggle='tooltip' data-placement='top' title='" + item.snippet.title + "'></a></div>");
+      });
+    } else {
+      $("#content").append("<h3 class='center'>No Results!</h3>");
+    }
+  })
 }
 
 function init() {
